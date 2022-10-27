649d8262d2c1f364eb98a98f0f7d973782895566
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-27 16:18:39 -0500

    created Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get() {
+                // date formatter copied from https://stackoverflow.com/a/11591900
+                return (((this.createdAt.getMonth() > 8) ? (this.createdAt.getMonth() + 1) : ('0' + (this.createdAt.getMonth() + 1))) + '/' + ((this.createdAt.getDate() > 9) ? this.createdAt.getDate() : ('0' + this.createdAt.getDate())) + '/' + this.createdAt.getFullYear())
+            }
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+module.exports = reactionSchema;
