785aedbc0c5d868b6f649b23fcb43dd7d075126e
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-27 16:10:45 -0500

    created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+// Thought document skeleton/schema
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            minLength: 1
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get() {
+                // date formatter copied from https://stackoverflow.com/a/11591900
+                return (((this.createdAt.getMonth() > 8) ? (this.createdAt.getMonth() + 1) : ('0' + (this.createdAt.getMonth() + 1))) + '/' + ((this.createdAt.getDate() > 9) ? this.createdAt.getDate() : ('0' + this.createdAt.getDate())) + '/' + this.createdAt.getFullYear())
+            }
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [Reaction]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+// field 'reactionCount' NOT stored in MongoDB
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function() {
+        return this.reactions.length;
+    });
+
+// mongoose model created
+const Thought = model('thought', thoughtSchema);
+
+// export Thought model
+module.exports = Thought;
