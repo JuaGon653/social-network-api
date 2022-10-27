390c3e83b3f3506b8e4c2dac1444e2ff2168f2f0
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-27 18:28:12 -0500

    implemented updating a user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,14 +10,31 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:userId', async (req, res) => {
-    try {
-        const user = await User.findOne({ _id: req.params.userId });
-        res.status(200).json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.route('/:userId')
+    .get(async (req, res) => {
+        try {
+            const user = await User.findOne({ _id: req.params.userId });
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    })
+    .put(async (req, res) => {
+        try {
+            if (await (req?.body?.email) && !/^([a-z0-9\_\.\-]+)\@([\da-z\.\-]+)\.([a-z\.]{2,6})$/i.test(req.body.email)) {
+                throw {message: "Please enter a valid email!"};
+            };
+            const updatedUser = await User
+                .findOneAndUpdate(
+                    { _id: req.params.userId },
+                    { $set: req.body },
+                    { new: true }
+                );
+            res.status(200).json(updatedUser);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
 
 router.post('/', async (req, res) => {
     try {
@@ -26,6 +43,6 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
