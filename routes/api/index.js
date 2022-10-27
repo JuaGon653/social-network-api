bce4a0395f2022272ae5fbf7e718e0ee5bffb720
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-27 16:37:13 -0500

    api folder index file

---
+++ routes/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+// use path '/api/users' to get to user routes
+router.use('/users', userRoutes);
+// use path '/api/thoughts' to get to thought routes
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
