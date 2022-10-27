0dc26bb0fbe52c2b7b6d76176e503042cd29f543
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-27 17:26:57 -0500

    implemented post route to create a user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -19,4 +19,13 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
