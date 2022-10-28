c9a137e227d48cf0be16ac0013fb83a2e81ebf11
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-28 14:18:17 -0500

    route to create a thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,13 +1,24 @@
 const router = require('express').Router();
-const { Thought } = require('../../models/Thought');
+const { Thought } = require('../../models/');
 
-router.get('/', async (req, res) => {
-    try {
-        const thoughts = await Thought.find();
-        res.status(200).json(thoughts);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+router.route('/')
+    .get(async (req, res) => {
+        try {
+            const thoughts = await Thought.find();
+            res.status(200).json(thoughts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    })
+    .post(async (req, res) => {
+        try {
+            const newThought = await Thought.create(req.body);
+            res.status(200).json(newThought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+
+
 
 module.exports = router;
