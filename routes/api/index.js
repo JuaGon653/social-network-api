b9ede81d4ffb40238a2141e7268b02d18958aee6
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-28 14:05:36 -0500

    inserted thought route

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,6 @@ const thoughtRoutes = require('./thoughtRoutes');
 // use path '/api/users' to get to user routes
 router.use('/users', userRoutes);
 // use path '/api/thoughts' to get to thought routes
-// router.use('/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
