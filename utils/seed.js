0197d8d75b0f5e731f8e0e84243ad03fde3886de
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-10-28 17:50:32 -0500

    adds thoughts to the database

--- utils/seed.js
+++ utils/seed.js
@@ -6,8 +6,10 @@ connection.on('error', (err) => err);
 connection.once('open', async () => {
     console.log('connected');
     await User.deleteMany({});
+    await Thought.deleteMany({});
 
     const users = [];
+    const thoughts = [];
 
     users.push({
         username: "JuaGon653",
@@ -20,6 +22,26 @@ connection.once('open', async () => {
 
     for (let user of users) {
         await User.create(user);
+    }
+
+    let id = await User.find();
+    thoughts.push({
+        thoughtText: "This is a thought...",
+        username: "JuaGon653",
+        userId: id[0]._id
+    });
+    thoughts.push({
+        thoughtText: "That's enough",
+        username: "JuaGon653",
+        userId: id[0]._id
+    });
+
+    for (let thought of thoughts)    {
+        const thoughttt = await Thought.create(thought);
+        await User.findOneAndUpdate(
+            { _id: thought.userId },
+            { $addToSet: { thoughts: thoughttt._id }},
+        );
     }
     process.exit(0);
 })
